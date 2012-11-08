9697a15482fffb63998b2a512a814355bcf26fcd
Pedro Matias <zydeon@gmail.com>
2012-11-08 21:11:01 +0000

    kajsbd

---
+++ src/DatabaseInitializer.java
@@ -0,0 +1,14 @@
+// import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.sql.*;
+// import java.util.Date;
+import dbconnect.Database;
+
+public class DatabaseInitializer extends HttpServlet {
+
+	public void init() throws ServletException{
+		Database.init();
+	}
+
+}
