925391f0589937045ee7540e85983c270e903acd
Pedro Matias <zydeon@gmail.com>
2012-11-21 22:39:08 +0000

    passar queries de ServletLogin para a a classe Database (MVC)

--- src/Login.java
+++ src/Login.java
@@ -17,44 +17,12 @@ public class Login extends HttpServlet {
 		// escape(user);
 		// escape(pass);
 
-		try{
-			Connection con = Database.getConnection();
-
-			if( con != null ){
-				Statement st = con.createStatement();
-				ResultSet rs;
-
-				sql = "SELECT salt FROM \"user\" WHERE login='"+user+"';";
-				rs = st.executeQuery(sql);
-				if(rs.next()){
-					String salt = rs.getString("salt");
-					String hash = Database.generateHash(pass, salt);
-
-					sql = "SELECT count(*) FROM \"user\" WHERE login='"+user+"' AND pass='"+hash+"'";
-					rs = st.executeQuery(sql);
-					if( rs.next() && rs.getInt("count")>0 ){
-						request.getSession(true).setAttribute("user", user);
-						response.sendRedirect("index.jsp");
-					}
-					else
-						response.sendRedirect("login.jsp?msg=Wrong username or password");
-				}	
-				else
-					response.sendRedirect("login.jsp?msg=Wrong username or password");
-
-				Database.putConnection(con);
-			}		
+		if( Database.authUser(user, pass) ){
+			request.getSession(true).setAttribute("user", user);
+			response.sendRedirect("index.jsp");			
 		}
-		catch(SQLException e){
-			System.out.println(e);
-		}
-		catch(java.security.NoSuchAlgorithmException e){
-			System.out.println(e);
-		}
-
-		try{
-			response.sendRedirect("login.jsp?msg=Problems with connection or database!");
-		}
-		catch(java.lang.IllegalStateException e){}		
+		else{
+			response.sendRedirect("login.jsp?msg=Wrong username or password");
+		}	
 	}	
 }
